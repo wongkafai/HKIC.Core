d9217c3a9331c75d7324b205072042fcb3dc79e3
wongkafai <wongkafai@msn.com>
2020-11-23 17:23:24 +0800

    update

--- HKIC.Core/Program.cs
+++ HKIC.Core/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Reflection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 
 namespace HKIC.Core
 {
@@ -14,7 +19,66 @@ namespace HKIC.Core
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == EnvironmentName.Development)
+            {
+                CreateWebHostBuilder(args).Build().Run();
+                return;
+            }
+            var host = new WebHostBuilder()
+               .UseKestrel(options =>
+               {
+                   options.Listen(IPAddress.Any, 8080);
+                   options.Limits.MaxConcurrentUpgradedConnections = 10 * 1024;
+                   options.Limits.MinRequestBodyDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(100));
+                   options.Limits.MinResponseDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(100));
+               })
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   var env = hostingContext.HostingEnvironment;
+
+                   config.AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
+                         .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                         .AddJsonFile($"config/appsettings.json", optional: true, reloadOnChange: true)
+                         .AddJsonFile($"secret/appsettings.json", optional: true, reloadOnChange: true);
+
+                   if (env.IsDevelopment())
+                   {
+                       var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                       if (appAssembly != null)
+                       {
+                           config.AddUserSecrets(appAssembly, optional: true);
+                       }
+                   }
+
+                   config.AddEnvironmentVariables();
+
+                   if (args != null)
+                   {
+                       config.AddCommandLine(args);
+                   }
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                    //logging.UseConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                   logging.AddDebug();
+               })
+               //.UseIISIntegration()
+               .UseDefaultServiceProvider((context, options) =>
+               {
+                   options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+               })
+               .ConfigureServices(services =>
+               {
+                   services.AddTransient<IConfigureOptions<KestrelServerOptions>, KestrelServerOptionsSetup>();
+               })
+               .UseStartup<Startup>()
+               .Build();
+
+
+            host.Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- HKIC.Core/Startup.cs
+++ HKIC.Core/Startup.cs
@@ -1,47 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace HKIC.Core
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            HostingEnvironment = environment;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Price Book Service API V1", Version = "v1" });
+
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            if (env.IsDevelopment())            
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service Token Service API V1");
+            });
+
+
             app.UseMvc();
         }
     }
